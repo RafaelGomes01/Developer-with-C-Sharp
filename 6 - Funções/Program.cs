4c3986c65150b05305a59ff96d75cb0a91ff5a60
RafaelGomes01 <53452997+RafaelGomes01@users.noreply.github.com>
2020-11-23 15:00:33 -0300

    Exercicios Criados

---
+++ 6 - Funções/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _6___Funções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 - Faça um programa que receba o nome e quatro notas de 10 alunos e armazene essas notas em vetores. Calcule e mostre:
+            // A. A Média do aluno - Função
+            // B. A quantidade de Aprovados (Média >=7)
+            // C. A quantidade de Reprovados (Méida < 7)
+            // D. A Média geral dos 10 alunos.
+        }
+    }
+}
