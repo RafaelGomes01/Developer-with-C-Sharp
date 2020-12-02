3b511955fe8aa5d14a858cf990b082d2db63ff0f
RafaelGomes01 <53452997+RafaelGomes01@users.noreply.github.com>
2020-12-02 10:56:17 -0300

    Exercicio POO

---
+++ 7 - POO em C#/Atributos e Metodos POO/Personagem.cs
@@ -0,0 +1,23 @@
+namespace Atributos_e_Metodos_POO
+{
+     public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+        public string iA = "Jarvis";
+        public int vida = 100;
+        public int Ataque1(){
+            return 100;
+        }
+        
+        public int Ataque2(){
+            return 20;
+        }
+
+        public int Defesa(int ataque){
+            this.vida = this.vida - ataque;
+            return this.vida = this.vida - ataque;
+        }
+    }
+}
---
+++ 7 - POO em C#/Atributos e Metodos POO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Atributos_e_Metodos_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Instaciando objeto
+        Personagem jogador1 = new Personagem();
+        jogador1.nome = "Rafael Gomes";
+        jogador1.idade = 17;
+        jogador1.armadura = "Mark1";
+        jogador1.iA = "Friday";
+
+        Personagem jogador2 = new Personagem();
+        jogador2.nome = "Peter Paker";
+        jogador2.idade = 21;
+        jogador2.armadura = "Spider armor";
+        
+        Console.Clear();
+        Console.WriteLine($"Partida = {jogador1.nome} VS {jogador2.nome}");
+        Console.WriteLine($"Idade Jogador 1: {jogador1.idade} || Idade Jogador 2: {jogador2.idade}");
+        Console.WriteLine($"Armadura Jogador 1: {jogador1.armadura} || Armadura Jogador 2: {jogador2.armadura}");
+        Console.WriteLine($"Jogador 1 IA: {jogador1.iA} || Jogador 2 IA: {jogador2.iA}");
+
+        int vidaDoJogador2 = jogador2.Defesa(jogador2.Ataque1());
+        if(vidaDoJogador2 <= 0){
+                Console.WriteLine("O jogador morreu");
+        }else{
+            Console.WriteLine($"Jogador 2 depois do ataque ficou com {jogador2.vida} de vida");
+        }
+        }
+    }
+}
